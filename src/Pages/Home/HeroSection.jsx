2a0a5231f44d1cf3267db3963c0e64c84fccb51c
waqasa1 <waqasanwar1308@gmail.com>
2025-05-12 18:30:31 +0500

    navbar fix

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -5,7 +5,7 @@ import hero from '../../assets/heroimage.webp'
 const HeroSection = () => {
     return (
         <>
-            {/* <Box sx={{ display: 'flex', flexDirection: 'row', maxWidth:'1500px', margin:'0 auto', marginTop:'20px', px:'50px' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', maxWidth:'1500px', margin:'0 auto', marginTop:'20px', px:'50px' }}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', my: 'auto' }}>
                     <Typography sx={{ fontSize: 'clamp(3rem, 4.5rem, 5vw)', lineHeight: '1em', letterSpacing: '-2px', marginBottom: '20px', fontWeight: '600', width: '700px' }}>We manage your IT, so you can manage your business.</Typography>
                     <Typography sx={{ fontSize: 'clamp(1.6rem, 2rem, 2.55vw)' }}>Take charge of your business continuity with innovative IT solutions</Typography>
@@ -17,7 +17,7 @@ const HeroSection = () => {
 
                 
             </Box>
-            <Divider sx={{py:'20px', width:'80%', margin:'0 auto'}}/> */}
+            <Divider sx={{py:'20px', width:'80%', margin:'0 auto'}}/>
         </>
     )
 }
