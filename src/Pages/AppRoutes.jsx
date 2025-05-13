bb96f4075016077eebad5e1d7dfea4e3abd82245
waqasa1 <waqasanwar1308@gmail.com>
2025-05-13 18:52:40 +0500

    Home fixed

--- src/Pages/AppRoutes.jsx
+++ src/Pages/AppRoutes.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout/Layout";
 import Contact from "./Contact/Form";
-import Home from './Home/HeroSection'
-import StarFooter from "./Layout/StarFoter";
+import Home from './Home/Home'
+// import StarFooter from "./Layout/StarFoter";
 import Recognized from "./Home/Recognized";
 
 const AppRoutes = () => {
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import Recognized from './Recognized'
+import Contact from '../Contact/Form'
+
+const Home = () => {
+  return (
+    <div>
+        <HeroSection />
+        <Recognized />
+        <Contact />
+    </div>
+  )
+}
+
+export default Home
