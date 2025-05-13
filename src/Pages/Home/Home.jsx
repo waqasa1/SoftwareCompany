6748e65920aa14fdd9cfef7627ce07fc073e65b9
waqasa1 <waqasanwar1308@gmail.com>
2025-05-13 18:29:48 +0500

    Home section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Contact from '../Contact/Form';
 import StarFooter from '../Layout/StarFoter';
+import HeroSection from './HeroSection'
 
 const HomeComponent = () => {
     return (
         <>
+        <HeroSection/>
         <Contact />
-        <StarFooter />
         </>
     );
 };
