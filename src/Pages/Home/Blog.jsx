e4acfbc1de7cd4d2013bd1cc0039b5a33d4db762
Tayyab-CodeLover <tayyabhussain505@gmail.com>
2025-05-14 14:51:56 +0500

    blog

---
+++ src/Pages/Home/Blog.jsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+const Blog = () => {
+    return(
+    <>
+    <Box sx={{display:"flex", justifyContent:"center"}}>
+        <Typography sx={{padding:"10px",width:"fit-content"}}>
+            BLOG
+        </Typography>
+    </Box>
+    </>
+     )
+}
+export default Blog
