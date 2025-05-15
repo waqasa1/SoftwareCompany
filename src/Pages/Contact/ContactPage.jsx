16b00ef78ee7913270fa82d01678b2c4ee9761c0
waqasa1 <waqasanwar1308@gmail.com>
2025-05-15 12:36:11 +0500

    contactpage added

--- src/Pages/Contact/ContactPage.jsx
+++ src/Pages/Contact/ContactPage.jsx
@@ -9,16 +9,22 @@ const ContactPage = () => {
                 <Box sx={{ width: { lg: '80%' }, margin: '0 auto' }}>
                     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                         <Button sx={{ color: 'gray', backgroundColor: 'rgb(245, 237, 237)', fontSize: '10px', fontWeight: '600', marginTop: '20px' }}>Contact</Button>
-                        <Typography sx={{ fontSize: { xs: '20px', lg: '48px' }, fontWeight: '700', letterSpacing: '-1px' }}>We’re here to help</Typography>
-                        <Box sx={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
+                        <Typography sx={{ fontSize: { xs: '20px', lg: '48px' }, fontWeight: '700', letterSpacing: '-1px', marginTop: '10px' }}>We’re here to help</Typography>
+                        <Box sx={{ display: 'flex', gap: '20px', alignItems: 'center', flexDirection: { xs: 'column', lg: 'row' }, marginBottom: '10px' }}>
                             <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                                 <Typography>Call us at:</Typography>
                                 <Typography> 1-800-356-8933</Typography>
                             </Box>
-                            <Box>
+                            <Box sx={{ display: { xs: 'none', lg: 'block' } }}>
                                 <svg width="24" height="80" viewBox="0 0 24 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M8 10L16 50L8 90" stroke="gray" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
+                            </Box>
+                            <Box sx={{ display: { xs: 'block', lg: 'none' } }}>
+                                <svg width="100" height="24" viewBox="0 0 100 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10 8L50 16L90 8" stroke="gray" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
                             </Box>
                             <Box sx={{ display: 'flex', flexDirection: 'column' }}>
                                 <Typography>Email us:</Typography>
@@ -26,35 +32,128 @@ const ContactPage = () => {
                             </Box>
                         </Box>
                         <Box><Button onClick={() => { const contactSection = document.getElementById('contact-form-section'); if (contactSection) { contactSection.scrollIntoView({ behavior: 'smooth' }); } }} variant="contained" sx={{ marginRight: '20px', marginBottom: { xs: '5px', lg: '0px' }, textTransform: 'none' }} >Schedule a Free Consultation</Button></Box>
-                        <Box sx={{ marginTop: '20px' }}><img src="https://i.imgur.com/LDkLFoa.gif" alt="" /></Box>
+                        <Box sx={{ marginTop: '20px', backgroundImage: 'url("https://tecnologia.vamtam.com/wp-content/uploads/2023/04/pexels-photomix-company-518244.jpg")', width: '1280px', height: '350px', maxWidth: '100%', backgroundSize: { xs: 'contain', lg: 'cover' }, backgroundRepeat: 'no-repeat' }}></Box>
                     </Box>
-                    <Box sx={{ marginTop: '20px' }}>
+                    <Box sx={{ marginTop: '20px', px: '10px' }}>
                         <Typography sx={{ fontSize: '48px', fontWeight: '700', color: '#0A0D31', letterSpacing: '0px' }}>Our Locations</Typography>
-                        <Typography sx={{ width: '40%', marginTop: '5px', fontSize: '20px', fontWeight: '400', color: 'gray', letterSpacing: '0px' }}>We have offices throughout the East Coast, Midwest, and South — we’d love to show you around sometime. Don’t see an office in your area? We have the power to support your business, no matter the location.</Typography>
+                        <Typography sx={{ width: { lg: '40%' }, marginTop: '5px', fontSize: '20px', fontWeight: '400', color: 'gray', letterSpacing: '0px' }}>We have offices throughout the East Coast, Midwest, and South — we’d love to show you around sometime. Don’t see an office in your area? We have the power to support your business, no matter the location.</Typography>
                     </Box>
                     {/*locations*/}
-                    <Box sx={{ display: 'flex', flexWrap: 'wrap', width: { lg: '80%' }, margin: '20px 0', marginBottom: '50px', marginTop:'100px' }}>
-                        <Box sx={{ width: { xs: '100%', lg: '20%' }, mb: 2 , borderRight: '1px solid gray',paddingRight:'80px'}}>
-                            <Typography sx={{fontSize: '30px', fontWeight: '600', color: '#0A0D31', letterSpacing: '0px'}}>Florida</Typography>
-                            <Typography sx={{fontSize: '20px', fontWeight: '600', letterSpacing: '0px'}}>Bonita Springs</Typography>
-                            <Typography sx={{fontSize: '20px', fontWeight: '400', letterSpacing: '0px',color:'gray'}}>28200 Old 41 Rd #208 Bonita Springs, FL 34135</Typography>
-                            <Button>(817) 575-6220</Button>
-                            <Button>Get direction</Button>
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap', width: { lg: '80%' }, margin: '20px 0', marginBottom: '50px', marginTop: '100px' }}>
+                        <Box sx={{ width: { xs: '100%', lg: '20%' }, mb: 2, borderRight: '1px solid gray', px: '50px', paddingLeft: { xs: '80px', lg: '' } }}>
+                            <Typography sx={{ fontSize: '30px', fontWeight: '600', color: '#0A0D31', letterSpacing: '0px' }}>Florida</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: '600', letterSpacing: '0px' }}>Bonita Springs</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: '400', letterSpacing: '0px', color: 'gray' }}>28200 Old 41 Rd #208 Bonita Springs, FL 34135</Typography>
+                            <Button href="tel:(817) 575-6220" sx={{
+                                color: 'gray', fontSize: '20px', fontWeight: '400', marginLeft: '-8px',
+                                ":hover": {
+                                    color: 'blue',
+                                    background: 'none'
+                                }
+                            }}>
+                                {'(817) 575-6220'}
+                            </Button>
+                            <Button sx={{
+                                fontSize: '12px', textTransform: 'none', color: 'blue', fontWeight: 'bold', marginLeft: '-5px',
+                                ":hover": {
+                                    color: 'black',
+                                    background: 'none'
+                                },
+                                '&::after': {
+                                    content: '""',
+                                    position: 'absolute',
+                                    left: 0,
+                                    bottom: '10px',
+                                    height: '1px',
+                                    width: '0%',
+                                    backgroundColor: 'black', // MUI primary color or custom
+                                    transition: 'width 0.3s ease-in-out',
+                                    marginLeft: '9px'
+                                },
+                                '&:hover::after': {
+                                    width: '80%',
+                                    marginLeft: '9px'
+                                },
+                            }}>
+                                Get direction
+                            </Button>
                         </Box>
                         <Divider sx={{ width: '80%', margin: '0 auto', mb: '20px', display: { xs: 'block', lg: 'none' } }} />
-                       <Box sx={{ width: { xs: '100%', lg: '20%' }, mb: 2 , borderRight: '1px solid gray',px:'80px'}}>
-                             <Typography sx={{fontSize: '30px', fontWeight: '600', color: '#0A0D31', letterSpacing: '0px'}}>Georgia</Typography>
-                             <Typography sx={{fontSize: '20px', fontWeight: '600', letterSpacing: '0px'}}>Atlanta</Typography>
-                            <Typography sx={{fontSize: '20px', fontWeight: '400', letterSpacing: '0px',color:'gray'}}>3565 Piedmont Rd NEBuilding 2, Suite 200 Atlanta GA 30222</Typography>
-                            <Button>(817) 575-6220</Button>
-                            <Button>Get direction</Button>
+                        <Box sx={{ width: { xs: '100%', lg: '20%' }, mb: 2, borderRight: '1px solid gray', px: '80px' }}>
+                            <Typography sx={{ fontSize: '30px', fontWeight: '600', color: '#0A0D31', letterSpacing: '0px' }}>Georgia</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: '600', letterSpacing: '0px' }}>Atlanta</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: '400', letterSpacing: '0px', color: 'gray' }}>3565 Piedmont Rd NEBuilding 2, Suite 200 Atlanta GA 30222 </Typography>
+                            <Button href="tel:(404) 551-52222" sx={{
+                                color: 'gray', fontSize: '20px', fontWeight: '400', marginLeft: '-8px',
+                                ":hover": {
+                                    color: 'blue',
+                                    background: 'none'
+                                }
+                            }}>
+                                {'(404) 551-52222'}
+                            </Button>
+                            <Button sx={{
+                                fontSize: '12px', textTransform: 'none', color: 'blue', fontWeight: 'bold', marginLeft: '-5px',
+                                ":hover": {
+                                    color: 'black',
+                                    background: 'none'
+                                },
+                                '&::after': {
+                                    content: '""',
+                                    position: 'absolute',
+                                    left: 0,
+                                    bottom: '10px',
+                                    height: '1px',
+                                    width: '0%',
+                                    backgroundColor: 'black', // MUI primary color or custom
+                                    transition: 'width 0.3s ease-in-out',
+                                    marginLeft: '9px'
+                                },
+                                '&:hover::after': {
+                                    width: '80%',
+                                    marginLeft: '9px'
+                                },
+                            }}>
+                                Get direction
+                            </Button>
                         </Box>
-                       <Box sx={{ width: { xs: '100%', lg: '20%' }, mb: 2 ,paddingLeft:'80px'}}>
-                             <Typography sx={{fontSize: '30px', fontWeight: '600', color: '#0A0D31', letterSpacing: '0px'}}>Kansas</Typography>
-                             <Typography sx={{fontSize: '20px', fontWeight: '600', letterSpacing: '0px'}}>Kansas City</Typography>
-                            <Typography sx={{fontSize: '20px', fontWeight: '400', letterSpacing: '0px',color:'gray'}}>12421 W. 151st St., Suite 100 Olathe, KS 66000</Typography>
-                            <Button>(817) 575-6220</Button>
-                            <Button>Get direction</Button>
+                        <Box sx={{ width: { xs: '100%', lg: '20%' }, mb: 2, paddingLeft: '80px' }}>
+                            <Typography sx={{ fontSize: '30px', fontWeight: '600', color: '#0A0D31', letterSpacing: '0px' }}>Kansas</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: '600', letterSpacing: '0px' }}>Kansas City</Typography>
+                            <Typography sx={{ fontSize: '20px', fontWeight: '400', letterSpacing: '0px', color: 'gray' }}>12421 W. 151st St., Suite 100 Olathe, KS 66000</Typography>
+                            <Button href="tel:(325) 221-9900" sx={{
+                                color: 'gray', fontSize: '20px', fontWeight: '400', marginLeft: '-8px',
+                                ":hover": {
+                                    color: 'blue',
+                                    background: 'none'
+                                }
+                            }}>
+                                {'(325) 221-9900'}
+                            </Button>
+                            <Button sx={{
+                                fontSize: '12px', textTransform: 'none', color: 'blue', fontWeight: 'bold', marginLeft: '-5px',
+                                ":hover": {
+                                    color: 'black',
+                                    background: 'none'
+                                },
+                                '&::after': {
+                                    content: '""',
+                                    position: 'absolute',
+                                    left: 0,
+                                    bottom: '10px',
+                                    height: '1px',
+                                    width: '0%',
+                                    backgroundColor: 'black', // MUI primary color or custom
+                                    transition: 'width 0.3s ease-in-out',
+                                    marginLeft: '9px'
+                                },
+                                '&:hover::after': {
+                                    width: '80%',
+                                    marginLeft: '9px'
+                                },
+                            }}>
+                                Get direction
+                            </Button>
                         </Box>
                     </Box>
                 </Box>
